8fab44fb0cd82e49d8788bdf04d2bf937f01c949
long.dotrong <long.dotrong@lazada.com>
2017-09-24 21:22:05 +0700

    Fix io.Reader

--- http.go
+++ http.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -15,31 +16,27 @@ func SetHandler(h http.Handler) {
 }
 
 func Get(uri string, output interface{}) *http.Response {
-	return Call(http.MethodGet, uri, "", output)
+	return Call(http.MethodGet, uri, nil, output)
 }
 
 func Post(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPost, uri, body, output)
+	return Call(http.MethodPost, uri, strings.NewReader(body), output)
 }
 
 func Put(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPut, uri, body, output)
+	return Call(http.MethodPut, uri, strings.NewReader(body), output)
 }
 
 func Patch(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPatch, uri, body, output)
+	return Call(http.MethodPatch, uri, strings.NewReader(body), output)
 }
 
 func Delete(uri string, output interface{}) *http.Response {
-	return Call(http.MethodDelete, uri, "", output)
+	return Call(http.MethodDelete, uri, nil, output)
 }
 
-func Call(method string, uri string, body string, output interface{}) *http.Response {
-	var buf *strings.Reader
-	if body != "" {
-		buf = strings.NewReader(body)
-	}
-	r := httptest.NewRequest(method, uri, buf)
+func Call(method string, uri string, body io.Reader, output interface{}) *http.Response {
+	r := httptest.NewRequest(method, uri, body)
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, r)
 	parseBody(w.Result(), output)
