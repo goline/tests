1d58bae174ce7ad06fd97483a6c0d0d387b41b71
Long Do <me@dotronglong.com>
2017-10-01 10:51:44 +0700

    Update http

--- http.go
+++ http.go
@@ -1,12 +1,11 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 )
 
 var handler http.Handler
@@ -19,24 +18,29 @@ func Get(uri string, output interface{}) *http.Response {
 	return Call(http.MethodGet, uri, nil, output)
 }
 
-func Post(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPost, uri, strings.NewReader(body), output)
+func Post(uri string, body interface{}, output interface{}) *http.Response {
+	return Call(http.MethodPost, uri, body, output)
 }
 
-func Put(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPut, uri, strings.NewReader(body), output)
+func Put(uri string, body interface{}, output interface{}) *http.Response {
+	return Call(http.MethodPut, uri, body, output)
 }
 
-func Patch(uri string, body string, output interface{}) *http.Response {
-	return Call(http.MethodPatch, uri, strings.NewReader(body), output)
+func Patch(uri string, body interface{}, output interface{}) *http.Response {
+	return Call(http.MethodPatch, uri, body, output)
 }
 
 func Delete(uri string, output interface{}) *http.Response {
 	return Call(http.MethodDelete, uri, nil, output)
 }
 
-func Call(method string, uri string, body io.Reader, output interface{}) *http.Response {
-	r := httptest.NewRequest(method, uri, body)
+func Call(method string, uri string, v interface{}, output interface{}) *http.Response {
+	body, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	r := httptest.NewRequest(method, uri, bytes.NewReader(body))
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, r)
 	parseBody(w.Result(), output)
